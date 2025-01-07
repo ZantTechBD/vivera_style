c301d9bc17458148cc23b846ee2f113a0f915f64
myselfAdul <myselfadul@gmail.com>
2025-01-07 23:25:11 +0600

    test

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -121,7 +121,7 @@ const Collection = () => {
             showFilter ? "" : "hidden"
           } sm:block`}
         >
-          <p className="mb-3 text-sm font-medium">CATEGORIES</p>
+          <p className="mb-3 text-sm font-medium">CATEGORIESs</p>
           <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
             <p className="flex gap-2">
               <input
