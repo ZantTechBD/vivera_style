d7aa563b471c37c49a973bafc347c60fd70783cc
myselfAdul <myselfadul@gmail.com>
2025-01-14 15:58:08 +0600

    no money

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import ShopContextProvider from './context/ShopContext.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import ShopContextProvider from "./context/ShopContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <ShopContextProvider>System is very tired and hungry!
-      {/* <App /> */}
+    <ShopContextProvider>
+      <App />
     </ShopContextProvider>
-  </BrowserRouter>,
-)
+  </BrowserRouter>
+);
